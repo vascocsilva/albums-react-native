439570fb58d3cfc76ec27ca59d6ad8474416ea29
Vasco Silva <vasco.silva@tekzenit.com>
2018-10-17 14:52:36 +0100

    Improve styles of album details

--- components/Album/Album.js
+++ components/Album/Album.js
@@ -1,27 +1,55 @@
 import React from 'react'
-import { StyleSheet, Text, Image } from 'react-native'
+import { StyleSheet, Text, Image, View } from 'react-native'
 import Card from '../Card'
 import CardSection from '../CardSection'
 
-const Album = ({ album: {title, artist, image} }) => {
-  const { imageStyle, viewStyle } = styles
+const Album = ({ album }) => {
+  const { title, artist, image } = album
+  const {
+    imageStyle,
+    viewStyle,
+    artistImageStyle,
+    imageContainerStyle,
+    headerStyle,
+    titleStyle,
+  } = styles
 
   return (
     <Card>
       <CardSection>
+        <View>
+          <Image
+            source={{uri: 'https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/256x256/singer.png'}}
+            style={artistImageStyle}
+          />
+        </View>
+        <View style={headerStyle}>
+          <Text style={titleStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
       <CardSection>
-        <Image source={{uri: image}} style={imageStyle} />
-        <Text>{title} - {artist}</Text>
+        <View style={imageContainerStyle}>
+          <Image source={{uri: image}} style={imageStyle} />
+        </View>
       </CardSection>
     </Card>
   )
 }
 
 const styles = {
+  imageContainerStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
   imageStyle: {
     width: 200,
     height: 200,
+  },
+  artistImageStyle: {
+    width: 40,
+    height: 40,
   },
   viewStyle: {
     display: 'flex',
@@ -29,6 +57,15 @@ const styles = {
     justifyContent: 'center',
     marginBottom: 15,
     marginTop: 15,
+  },
+  headerStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    marginLeft: 10,
+  },
+  titleStyle: {
+    fontSize: 20,
+    fontWeight: 'bold',
   }
 }
 
